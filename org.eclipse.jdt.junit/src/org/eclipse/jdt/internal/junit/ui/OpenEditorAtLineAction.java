dc87b4feb3fa466a7636f8206a2317376551cc5d
daniel@belteshazzar.com <daniel@belteshazzar.com>
2015-12-15 23:48:34 +1030

    setup for proper dev

--- org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/ui/OpenEditorAtLineAction.java
+++ org.eclipse.jdt.junit/src/org/eclipse/jdt/internal/junit/ui/OpenEditorAtLineAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2010, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,7 @@
  *     IBM Corporation - initial API and implementation
  *     Sebastian Davids: sdavids@gmx.de bug 37333 Failure Trace cannot
  * 			navigate to non-public class in CU throwing Exception
+ *     Daniel Walton: daniel@belteshazzar.com Non-Java files.
  *******************************************************************************/
 package org.eclipse.jdt.internal.junit.ui;
 
